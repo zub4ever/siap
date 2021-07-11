5ec9deced6231726a2591429a923cc588e9b475f
zub4ever <sklpb@outlook.com>
2021-07-11 14:50:00 -0500

    Ajustes fr

--- database/script/dados.sql
+++ database/script/dados.sql
@@ -1,10 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- * Author:  sklpb
- * Created: 11/07/2021
- */
 
+
+INSERT INTO sexo(id,nm_sexo) values(1,'Masculino'),(2,'Feminino');
+
+INSERT INTO marital_status(id,status_civil)values (1,'Solteiro'), (2, 'Casado');
+
+INSERT INTO obito(id,nm_obito)values(1,'Não');
+
+INSERT INTO origin(id,nm_origem)values(1,'Assembleia Legislativa');
+
+INSERT INTO orgao_expedidor(id,nm_orgao)values(1,'SSP/AC');
+
+INSERT INTO type_serve(id,status_servidor) values (1,'Ativo');
