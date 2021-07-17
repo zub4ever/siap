98428606d4d80594eef00f871b2780f55a7a4898
zub4ever <sklpb@outlook.com>
2021-07-16 21:37:35 -0500

    Ajustes

---
+++ database/migrations/2021_07_17_023211_create_contract_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContractTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('contract', function (Blueprint $table) {
+            $table->increments('id');
+            //
+            $table->integer('origin_id')->unsigned();
+            $table->foreign('origin_id')->references('id')->on('origin')->onDelete('cascade');
+            //
+            $table->integer('orgao_id')->unsigned();
+            $table->foreign('orgao_id')->references('id')->on('orgao')->onDelete('cascade');
+            //
+            $table->integer('funcao_id')->unsigned();
+            $table->foreign('funcao_id')->references('id')->on('funcao')->onDelete('cascade');
+            //
+            $table->integer('serve_id')->unsigned();
+            $table->foreign('serve_id')->references('id')->on('serve')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('contract');
+    }
+
+}
