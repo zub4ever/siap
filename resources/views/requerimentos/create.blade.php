6e12935b437e4aa99974433235135f360256016b
alisson.nascimento <alisson.nascimento@ac.gov.br>
2022-01-12 09:07:14 -0500

    SideBar cor

--- resources/views/requerimentos/create.blade.php
+++ resources/views/requerimentos/create.blade.php
@@ -150,8 +150,7 @@
 
 
         <script type='text/javascript' src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js'></script>
-        <script type='text/javascript' src=''></script>
-        <script type='text/javascript' src=''></script>
+        
         <script type='text/Javascript'>
             var currentTab = 0;
             document.addEventListener("DOMContentLoaded", function(event) {
