903f129398b0d9cf81814d511edc1485979317b3
zub4ever <sklpb@outlook.com>
2022-09-23 13:10:05 -0500

    Ajustes

--- resources/views/daf/almoxarifado/consultaPublica/resultadoqrCode.blade.php
+++ resources/views/daf/almoxarifado/consultaPublica/resultadoqrCode.blade.php
@@ -6,23 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.png')}}">
 </head>
 <body>
 <div class="container-fluid p-5 bg-primary text-white text-center">
     <img src="/imagem/logo2.png" height="100" width="100">
 </div>
-<div class="row">
-    <div class="col-xs-6 col-md-4">
-    </div>
-    <div class="col-xs-6 col-md-4">
-        <h4>
-            <br>
-            <h2>Consulta de Almoxarifado</h2>
-        </h4>
-    </div>
-    <div class="col-xs-6 col-md-4"></div>
+<br><br>
+<div class="centercard">
+    <h4>Consulta de Almoxarifado</h4>
 </div>
 <hr>
+<br><br>
 <div class="container mt-3">
     <table class="table table-striped">
         <thead>
@@ -64,6 +59,11 @@
         width: 100px;
 
     }
+    .centercard {
+        text-align: center;
+        border: 3px solid blue;
+    }
+
 </style>
 </body>
 </html>
