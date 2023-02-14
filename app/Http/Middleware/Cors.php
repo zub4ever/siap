414c533707299c075db971f8e6d735536ff2d000
zub4ever <sklpb@outlook.com>
2023-02-13 20:19:43 -0500

    Ajuestes API

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,9 +17,12 @@ class Cors
    public function handle($request, Closure $next)
 {
     return $next($request)
+            
         ->header('Access-Control-Allow-Origin', '*')
+        
         ->header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization');        
+        ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token, Authorization');   
+    
 }
 
 
